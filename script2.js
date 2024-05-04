3479a2b219fab853dd5432f48078618ffc49bd39
addtohart <168731098+addtohart@users.noreply.github.com>
2024-05-04 22:31:36 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,3 +4,41 @@ const searchBar = document.getElementById("searchBar");
 searchIcon.addEventListener("click", function() {
   searchBar.classList.toggle("active");
 });
+.about {
+    padding: 50px 0;
+    background-color: #F4F4F4;
+}
+
+.container {
+    display: flex;
+    align-items: center;
+    width: 80%;
+    margin: auto;
+}
+
+.about-video {
+    flex: 1;
+}
+
+.about-video video {
+    max-width: 100%;
+    height: auto;
+    display: block;
+}
+
+.about-text {
+    flex: 1;
+}
+
+.about-text h1 {
+    font-size: 300%;
+}
+
+.about-text p {
+    font-family: Bahnschrift Light;
+    color: #4A446A;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 1.5;
+}
+
